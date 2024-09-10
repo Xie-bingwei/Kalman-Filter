3a07217616150cb25263308d39f59c0fae5abb74
白玉京 <53562464+Xie-bingwei@users.noreply.github.com>
2024-09-10 12:14:32 +0800

    Update Kalman.h

--- Kalman_file/Kalman.h
+++ Kalman_file/Kalman.h
@@ -1,16 +1,25 @@
+#ifndef KALMAN_FILTERING_KALMAN_H
+#define KALMAN_FILTERING_KALMAN_H
+
 typedef struct Kalman_filter
 {
-    float Last_prediction_cov; /* (k|k-1)*/
-    float System_state_prediction_matrix; /*列矩阵*/
+    float C_last; /*上次预测过程协方差矩阵 C(k|k-1)*/
+    float X_last; /*系统状态预测矩阵，列矩阵*/
 
-    float Process_noise_cov;
-    float Measure_noise_cov;
+    float Q; /*过程噪声协方差*/
+    float R; /*量测噪声协方差*/
 
-    float K;                                                  /*卡尔曼增益，列矩阵*/
-    float Optimal_output; /*最优估计输出矩阵，列矩阵*/
-    float Optimal_cov_matrix; /*最优估计协方差矩阵C(k|k)*/
+    float K; /*卡尔曼增益，列矩阵*/
+    float X; /*估计输出矩阵，列矩阵*/
+    float C; /*估计协方差矩阵C(k|k)*/
 
     float input; /*量测值，即Z(k)*/
 } kal_filter;
 
+extern float angle, angle_dot;
 void Kalman_Filter(float Accel, float Gyro);
+void First_order_filter(float angle_m, float gyro_m);
+float kalman_filter(kal_filter* k_flt, float input);
+
+#endif //KALMAN_FILTERING_KALMAN_H
+
